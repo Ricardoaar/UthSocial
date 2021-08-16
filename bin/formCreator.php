5a1714e7521809d929445cecdb8d1c273e690947
caprun <26richardr@gmail.com>
2021-08-16 13:40:31 -0500

    Fixed maybe pleaseeeeee

--- bin/formCreator.php
+++ bin/formCreator.php
@@ -11,6 +11,14 @@ $answers = [];
 
 $formId = $formCrud->insert(['name' => $title, 'id_category' => 1]);
 $open = [];
+function endsWith($haystack, $needle): bool
+{
+    $length = strlen($needle);
+    if (!$length) {
+        return true;
+    }
+    return substr($haystack, -$length) === $needle;
+}
 
 foreach ($data as $key => $item) {
     if (startsWith($key, 'question')) {
@@ -21,27 +29,33 @@ foreach ($data as $key => $item) {
     }
 }
 
-var_dump($answers);
 
 $ids = [];
 
 foreach ($questions as $question => $value) {
     $type = substr($question, strlen('question1'));
-    if ($type === "") {
+    $type = "";
+    if (endsWith($question, 'num')) {
+        $type = 'num';
+    } else if (endsWith($question, 'text')) {
+        $type = 'text';
+    } else {
         $type = 'mult';
     }
     $object = ['question' => $value, 'id_form' => $formId, 'type' => $type];
-
     $id = $questionCrud->get();
-
     $questionCrud->insert($object);
     $ids[$question[strlen('question')]] = $questionCrud->getMaxId();
 }
+
 foreach ($answers as $answer => $value) {
-    $ansId = $ids[substr($answer, strlen('answer1question'))];
-    $obj = ['answer' => $value, 'id_question' => $ansId];
+
+    $position = strpos($answer, 'on');
+    $questID = $ids[substr(substr($answer, $position), 2)];
+    $obj = ['answer' => $value, 'id_question' => $questID];
     $answersCrud->insert($obj);
 }
+
 function startsWith($text, $needle): bool
 {
     return (substr($text, 0, strlen($needle)) === $needle);
