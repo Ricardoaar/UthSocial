2c9a3313b650072b43e51c6978599fff6ef5be43
caprun <26richardr@gmail.com>
2021-08-21 01:10:41 -0500

    analyst enhance

--- public/analystForm.php
+++ public/analystForm.php
@@ -8,16 +8,35 @@ require('../partials/header.php');
             <div class="col">
                 <?php
                 require('../bin/Crud.php');
-                require('../bin/helpers/HtmlBuilder.php');
+
+                echo "<table class=\"table table-responsive-md\"><tr>";
+                echo "<th scope='col'>Id</th>";
+                echo "<th scope='col'>Pregunta</th>";
+                echo "<th scope='col'>Ver</th>";
+                echo "</tr></thead><tbody>";
+
                 $id = $_REQUEST['id'];
+                $con = new Connection();
+                $connection = $con->connect();
+                $sql = "SELECT id,question FROM question where id_form=$id";
 
-                $questCrud = new Crud('form_display');
-                $questCrud->where_and("id", "=", intval($id));
+                $quest = $connection->prepare($sql);
+                $quest->execute();
+                $question_data = $quest->fetchAll(PDO::FETCH_ASSOC);
+                $questions = [];
+                foreach ($question_data as $question) {
+                    $quest_id = $question['id'];
+                    $questions[$quest_id] = $question['question'];
+                }
 
-                $data = $questCrud->get();
-                $headers = ['Form Id', 'Pregunta', 'Respuesta'];
-                $config = ["tr" => 'xl', "tc" => "thead-dark"];
-                echo buildTableStdAsString($headers, $data, $config);
+                foreach ($questions as $qid => $question) {
+                    echo "<th scope='col'>$qid</th>";
+                    echo "<th scope='col'>$question</th>";
+                    echo "<th scope='col'><a href='question.php?id=$qid&f=$id'><img height='20px' src='../assets/editar.png' alt=''></a></th>";
+                    echo "</tr>";
+                }
+                echo "</tbody></table>";
+
                 ?>
 
             </div>
---
+++ public/question.php
@@ -0,0 +1,65 @@
+<?php
+require('../partials/header.php');
+?>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <?php
+                require('../bin/Crud.php');
+                $id = $_REQUEST['id'];
+                echo "<table class=\"table table-responsive-md\"><tr>";
+                echo "<th scope='col'>#</th>";
+                echo "<th scope='col'>Respuesta</th>";
+                echo "<th scope='col'>Repeticiones</th>";
+                echo "</tr></thead><tbody>";
+
+                $con = new Connection();
+                $connection = $con->connect();
+                $sql = "SELECT answer FROM answer_storage where id_question=$id";
+                $ans = $connection->prepare($sql);
+                $ans->execute();
+                $ans_data = $ans->fetchAll(PDO::FETCH_ASSOC);
+                $answers = [];
+                foreach ($ans_data as $item) {
+                    foreach ($item as $ans) {
+                        array_push($answers, $ans);
+                    }
+                }
+                $count = 1;
+                foreach ($answers as $answer) {
+                    $sql = "SELECT COUNT(*) as val FROM answer WHERE answer='$answer'";
+                    $ans = $connection->prepare($sql);
+                    $ans->execute();
+                    $ans_data = $ans->fetchAll(PDO::FETCH_ASSOC);
+                    $value = $ans_data[0]['val'];
+                    echo "<th scope='col'>$count</th>";
+                    echo "<th scope='col'>$answer</th>";
+                    echo "<th scope='col'>$value</th>";
+                    echo "</tr>";
+//
+                    $count++;
+                }
+
+
+                //                foreach ($answers as $qid => $question) {
+                //                    echo "<th scope='col'>$question</th>";
+                //                    echo "<th scope='col'><a href='question.php?id=$qid'><img height='20px' src='../assets/editar.png' alt=''></a></th>";
+                //
+                //                }
+                echo "</tbody></table>";
+
+                ?>
+
+            </div>
+        </div>
+        <?php $id_f = $_REQUEST['f'];
+        echo "<a href='analystForm.php?id=$id_f'' class='btn btn-danger'>Regresar</a>"
+        ?>
+    </div>
+
+</main>
+<?php
+require('../partials/footer.php');
+?>
